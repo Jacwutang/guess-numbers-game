6db374c6c5a9d2f756c72b2de259c6f5ba772601
admin <jack@jacks-MacBook-Air.local>
2024-08-09 22:00:30 -0700

    add test script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create venv if not already exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+source venv/bin/activate
+
+# Install packages
+if [ -f "requiremements.txt" ]; then
+    pip install -r requirements.txt
+fi
+
+
+
+# Run tests
+pytest
