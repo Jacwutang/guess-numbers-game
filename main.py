f236a0898780698504d04f30d27b5d2fe2709b2b
admin <jack@Jacks-MacBook-Pro.local>
2024-08-16 22:06:00 -0700

    add exception handling

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import random, argparse
 
 def create_game(min_range: int, max_range: int) -> int:
     
-    winner = random.randrange(min_range, max_range + 1, 1)
+    while True:
+        try:
+            winner = random.randrange(min_range, max_range + 1, 1)
     
-    guess = None
-    while guess != winner:
-        guess = int(input(f'Enter a number between {min_range}-{max_range}: '))
+            guess = 0
+            while guess != winner:
+                guess = int(input(f'Enter a number between {min_range}-{max_range}: '))
 
-        if guess < winner:
-            print("Too low")
-        elif guess > winner:
-            print("Too high")
-        else:
-            print("Correct!")
+                if guess < winner:
+                    print("Too low")
+                elif guess > winner:
+                    print("Too high")
+                else:
+                    print("Correct!")
             return guess
+        except ValueError as e:
+            print(e)
 
-
+    
 if __name__ == "__main__":
     create_game(1,10)
 
